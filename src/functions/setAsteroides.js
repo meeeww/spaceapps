c66b6dacb3b8cae4184a98170fe8bcf8d41874ae
meeeww <thezasm03@gmail.com>
2023-10-06 22:16:08 +0200

    set Asteroide

--- src/functions/setAsteroides.js
+++ src/functions/setAsteroides.js
@@ -1,8 +1,17 @@
 import getApi from "../utils/getApi.js";
+import Asteroide from "../objects/Asteroide.js"
 
 // await getApi().then((respuesta) => { console.log(respuesta)})
-// const array = []
+const array = []
 
 getApi().then((respuesta) => {
-    console.log(respuesta)
+    console.log(respuesta["near_earth_count"].map((dias) => {
+        dias.map((asteroide) => {
+            let Asteroide_API = new Asteroide;
+            Asteroide_API.setDireccion(asteroide["id"])
+
+            //
+            Array.push(Asteroide_API)
+        })
+    }))
 });
---
+++ src/functions/setMundo.js
@@ -0,0 +1,5 @@
+async function crearMundo() {
+    
+}
+
+export default crearMundo;
