8392963808f7f44a1d9312fac93356b1b808d7b3
meeeww <thezasm03@gmail.com>
2023-10-06 23:14:21 +0200

    Push mundo

--- src/functions/setMundo.js
+++ src/functions/setMundo.js
@@ -1,5 +1,8 @@
-async function crearMundo() {
-    
+import Mundo from "../objects/Mundo.js";
+
+async function crearMundo(nombre, imagen, tamaño) {
+  let mundo = new Mundo(nombre, imagen, tamaño);
+  return mundo;
 }
 
 export default crearMundo;
