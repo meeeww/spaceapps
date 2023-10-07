adacce1ca30fdf4f2d6ecd900263a4f5120e4754
Rakions <ogmohammed2004@hotmail.com>
2023-10-07 08:33:58 +0200

    creada funcionalidad score y niveles

--- src/functions/puntajes.js
+++ src/functions/puntajes.js
@@ -2,6 +2,9 @@ export default function puntajes() {
   var incremento = 0;
   var nivel = 1;
   setInterval(function () {
+    if (incremento % 20 == 0 && incremento >= 20) {
+      document.getElementById("indicadorNivel").innerHTML = "Nivel: " + nivel++;
+    }
     incremento++;
     document.getElementById("score").innerHTML = "Score: " + incremento;
   }, 1000);
--- src/functions/startGame.js
+++ src/functions/startGame.js
@@ -1,5 +1,4 @@
 document.querySelector(".menuPrincipal").addEventListener("click", () => {
-  console.log("hola");
   document.querySelector(".infoPartida").classList.add("active");
   document.getElementById("presionaJugar").style.display = "none";
   import("./puntajes.js").then((module) => {
