e26c578ced886e634b8557ff88cf23264a10cf4e
Rakions <ogmohammed2004@hotmail.com>
2023-10-07 08:40:21 +0200

    haciendo funcion para parar partida

--- src/functions/startGame.js
+++ src/functions/startGame.js
@@ -1,7 +1,13 @@
+let clicked = false;
 document.querySelector(".menuPrincipal").addEventListener("click", () => {
-  document.querySelector(".infoPartida").classList.add("active");
-  document.getElementById("presionaJugar").style.display = "none";
-  import("./puntajes.js").then((module) => {
-    module.default();
-  });
+  if (!clicked) {
+    clicked = true;
+    document.querySelector(".infoPartida").classList.add("active");
+    document.getElementById("presionaJugar").style.display = "none";
+    import("./puntajes.js").then((module) => {
+      module.default();
+    });
+  }
 });
+
+function stopGame() {}
