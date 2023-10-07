b2e20ff35a952c14776c8e9e66894df876313779
Ocejo <alejandro.ocejo@dxc.com>
2023-10-07 02:09:04 +0200

    test

--- src/objects/Asteroide.js
+++ src/objects/Asteroide.js
@@ -1,58 +1,66 @@
 export default class Asteroide {
-    nombre;
-    imagen;
-    tamaño;
-    velocidad;
-    direccion;
-  
-    constructor(nombre, imagen, tamaño, velocidad, direccion) {
-      this.nombre = nombre;
-      this.imagen = imagen;
-      this.tamaño = tamaño;
-      this.velocidad = velocidad;
-      this.direccion = direccion;
-    }
-  
-    getNombre() {
-      return this.nombre;
-    }
-  
-    setetNombre(nombre) {
-      this.nombre = nombre;
-    }
-  
-    getImagen() {
-      return this.imagen;
-    }
-  
-    setImagen(imagen) {
-      this.imagen = imagen;
-    }
-  
-    getTamaño() {
-      return this.tamaño;
-    }
-  
-    setTamaño(tamaño) {
-      this.tamaño = tamaño;
-    }
-  
-    getVelocidad() {
-      return this.velocidad;
-    }
-  
-    setVelocidad(velocidad) {
-      this.velocidad = velocidad;
-    }
-  
-    getDireccion() {
-      return this.Direccion;
-    }
-  
-    setDireccion(Direccion) {
-      this.Direccion = Direccion;
-    }
+  nombre;
+  imagen;
+  tamaño;
+  velocidad;
+  direccion;
+  id;
+
+  constructor(nombre, imagen, tamaño, velocidad, direccion, id) {
+    this.nombre = nombre;
+    this.imagen = imagen;
+    this.tamaño = tamaño;
+    this.velocidad = velocidad;
+    this.direccion = direccion;
+    this.id = id;
   }
-  
-  
-  /* yQn0L1akGvnnJ1pDfFsF5qNXtJLxUOR3AOVWUkeo */
+
+  getNombre() {
+    return this.nombre;
+  }
+
+  setNombre(nombre) {
+    this.nombre = nombre;
+  }
+
+  getImagen() {
+    return this.imagen;
+  }
+
+  setImagen(imagen) {
+    this.imagen = imagen;
+  }
+
+  getTamaño() {
+    return this.tamaño;
+  }
+
+  setTamaño(tamaño) {
+    this.tamaño = tamaño;
+  }
+
+  getVelocidad() {
+    return this.velocidad;
+  }
+
+  setVelocidad(velocidad) {
+    this.velocidad = velocidad;
+  }
+
+  getDireccion() {
+    return this.Direccion;
+  }
+
+  setDireccion(Direccion) {
+    this.Direccion = Direccion;
+  }
+  getId() {
+    return this.id;
+  }
+
+  setId(id) {
+    this.id = id;
+  }
+}
+
+/* yQn0L1akGvnnJ1pDfFsF5qNXtJLxUOR3AOVWUkeo */
